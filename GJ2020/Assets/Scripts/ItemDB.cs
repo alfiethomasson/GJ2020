c7dd3a5a8cd42b3699d6e8251ca59b8be4ac1c84
Alfie Thomasson <alfiethomasson0@gmail.com>
2020-02-01 00:04:02 +0000

    Adding unity maybe

---
+++ GJ2020/Assets/Scripts/ItemDB.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDB : MonoBehaviour
+{
+
+    public List<Item> items = new List<Item>();
+
+    // Start is called before the first frame update
+   void BuildDB()
+   {
+       items = new List<Item>(new Item(0, "Spare Part", "A rusty old part from a long forgotten era."));
+   }
+
+  private void Awake() {
+       {
+           BuildDB();
+       }
+   }
+}
