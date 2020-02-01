24f6161a89f09739006ddcc2b2345f28fa24ab6b
killeroid356 <34097198+killeroid356@users.noreply.github.com>
2020-02-01 15:38:51 +0000

    Added text alerts

---
+++ GJ2020/Assets/Scripts/QuestController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using TMPro;
+
+public class QuestController : MonoBehaviour
+{
+    
+    public static int QuestCounter = 0;
+    public TextMeshProUGUI text;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GJ2020/Assets/Scripts/TextFade.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TextFade : MonoBehaviour
+{
+    bool processing = false;
+    public TextMeshProUGUI text;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (text.text != "") {
+            if (processing)
+            { return; }
+            else {
+                processing = true;
+                StartCoroutine(Text());
+            }
+        }
+    }
+
+    IEnumerator Text() {
+        yield return new WaitForSeconds(5);
+        text.text = "";
+    }
+}
