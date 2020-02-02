e23d1cbf12c30bda8e66dc4541dbdb0c3c8fdf94
killeroid356 <34097198+killeroid356@users.noreply.github.com>
2020-02-02 14:10:35 +0000

    sign script

---
+++ GJ2020/Assets/Scripts/SignScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignScript : MonoBehaviour
+{
+
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 13)
+        {
+            SceneManager.LoadScene("Game Scene");
+        }
+
+
+    }
+
+   
+}
