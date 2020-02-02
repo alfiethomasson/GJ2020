838a496b73b9bc32d0bb36002f64a2ad47c0bbbe
killeroid356 <34097198+killeroid356@users.noreply.github.com>
2020-02-02 14:02:24 +0000

    working slime

--- GJ2020/Assets/Scripts/SlimeScript.cs
+++ GJ2020/Assets/Scripts/SlimeScript.cs
@@ -5,27 +5,57 @@ using UnityEngine;
 public class SlimeScript : MonoBehaviour
 {
     // Start is called before the first frame update
-
+    
     public GameObject player;
+    public GameObject bullet;
 
         RaycastHit hit;
+    int layermask = 1 << 13;
+   
 
+    void Start()
+    {
+        //Vector3 bulletpos = new Vector3(transform.position.x + 5, transform.position.y, 0.0f);
+        StartCoroutine(AI());
+    }
 
     // Update is called once per frame
     void Update()
     {       
-        Vector3 playerDir = player.transform.position - transform.position;
+        //Vector3 playerDir = player.transform.position - transform.position;
+        ////playerDir.Normalize();
 
-        if(Physics.Raycast(transform.position, playerDir, out hit, 50.0f));
+        //if(Physics.Raycast(transform.position, playerDir, out hit, 20, layermask));
+        //{
+        //    Debug.DrawRay(transform.position, playerDir, Color.green);
+        //  //  if(hit.collider != null)
+        //   // {
+        //    //if(hit.gameObject.tag == "Player")
+        //    //{
+        //     //   Debug.Log("working");
+        //   // }
+        //   // }
+        //}
+    }
+
+    IEnumerator AI()
+    {
+        Vector3 bulletpos = new Vector3(transform.position.x - 5, transform.position.y, 0.0f);
+        while (true)
         {
-            Debug.DrawRay(transform.position, playerDir, Color.green);
-          //  if(hit.collider != null)
-           // {
-            if(hit.gameObject.tag == "Player")
+            yield return new WaitForSeconds(2);
+            Vector3 playerDir = player.transform.position - transform.position;
+            //playerDir.Normalize();
+
+            if (Physics.Raycast(transform.position, playerDir, out hit, 20, layermask)) ;
             {
-                Debug.Log("working");
+
+                Debug.DrawRay(transform.position, playerDir, Color.green);
+                GameObject b = Instantiate(bullet, bulletpos, transform.rotation);
+                playerDir.Normalize();
+                b.GetComponent<Rigidbody2D>().velocity = playerDir*6.0f;
             }
-           // }
         }
     }
+
 }
